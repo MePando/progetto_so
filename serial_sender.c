9e3c0710e1f7594ca1a8ef0d82da92ddb6f3a5ea
Mario Edoardo Pandolfo <pandolfo.1835189@studenti.uniroma1.it>
2021-09-16 12:16:46 +0200

    pulito l'estrattore del comando

--- serial_sender.c
+++ serial_sender.c
@@ -2,55 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
-  if (argc<3) {
-    printf("usage: %s <filename> <baudrate>", argv[0]);
-    return 0;
-  }
-  char* filename=argv[1];
-  int baudrate=atoi(argv[2]), pos_x = 0, pos_y = 0;
-  char command[8];
-
-  printf( "opening serial device [%s] ... ", filename);
-  int fd=serial_open(filename);
-  if (fd<=0) {
-    printf ("Error\n");
-    return 0;
-  } else {
-    printf ("Success\n");
-  }
-  printf( "setting baudrate [%d] ... ", baudrate);
-
-  int attribs=serial_set_interface_attribs(fd, baudrate, 0);
-  if (attribs) {
-    printf("Error\n");
-    return 0;
-  }
-
-  serial_set_blocking(fd, 1);
-
-  const int bsize=8;
-  char buf[bsize];
-  while (1) {
-    printf("\nPlease write new camera position, using the format x:y in degrees from 0 to 180 (e.g. 90:180): ");
-	while (1){
-	 	scanf("%s", command);
-	        if(sscanf(command, "%d:%d", &pos_x, &pos_y)){
-
-			if((pos_x<=180 && pos_x>=0) && (pos_y<=180 && pos_y>=0)){
-				break;
-			}
-		}
-		printf("\nSome error please follow the format! Retry: ");
+int main(int argc, char** argv){
+	if(argc<3){
+		printf("usage: %s <filename> <baudrate>", argv[0]);
+		return 0;
 	}
-	  
-	printf("Thanks! Trying to send: %s\n", command);
-	
-	int n_write = write(fd, command, 8);
 
-	int n_read=read(fd, buf, bsize);
-    	for (int i=0; i<n_read; ++i) {
-   		printf("%c", buf[i]);
-   	}
-  }
+	char* filename = argv[1];
+	int baudrate = atoi(argv[2]);
+	
+	printf("Opening serial device [%s] ... ", filename);
+	int fd = serial_open(filename);
+	if(fd <= 0){
+		printf("Error\n");
+		return 0;
+	}else{
+		printf("Success\n");
+	}
+	
+	printf("Setting baudrate [%d] ...", baudrate);
+	int attribs = serial_set_interface_attribs(fd, baudrate, 0);
+	if(attribs){
+		printf("Error\n");
+		return 0;
+	}else{
+		printf("Success\n");
+	}
+
+	serial_set_blocking(fd, 1);
+
+	char command[7];
+	int pos_x, pos_y;
+
+	while(1){
+		printf("\nPlease write new camera position, using the format x:y in degrees from 0 to 180 (e.g. 90:180): ");
+		while(1){
+			scanf("%s", command);
+			if(sscanf(command, "%d:%d", &pos_x, &pos_y)){
+				if((pos_x <= 180 && pos_x >= 0) && (pos_y <= 180 && pos_y >= 0)){
+					break;
+				}
+			}
+			printf("Some error please follow the format! Retry: ");
+		}
+
+		printf("Thanks! Trying to send: %d and %d\n", pos_x, pos_y);
+	}
 }
